ab265447ee32f77d577f3f628921e2d6f4e95a68
DiegoSP15 <95321715+DiegoSP15@users.noreply.github.com>
2022-01-31 21:39:36 -0300

    scrip2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,48 @@
+// presentacion
+function persona (){
+    var nombre = prompt ("Cual es tu nombre")
+    var apellido = prompt ("Cual es tu apellido")
+    var cel = prompt ("Cual es tu numero de contacto")
+    var email = prompt ("Cual es tu email")
 
-let cantpers = prompt("¿para cuantas personas estima realizar el evento?" )
+    alert("Bienvenid@ " + nombre +" "+ apellido)
+}
+
+persona ();
+
+// tipo de evento
+let evento = prompt("¿Que tipo de evento desea realizar? Opciones: boda, cumpleaño, empresarial, discoteca" );
+
+if(evento == "boda"){
+    alert ("contamos con los mejores profesionales para realizar la boda de su sueños")
+}
+else if (evento == "cumpleaños"){
+    alert ("contamos con los mejores profesionales para realizar el cumpleaños de su sueños")
+}
+else if (evento == "empresarial"){
+    alert ("contamos con los mejores profesionales para realizar el evenot de su sueños")
+}
+else if (evento == "discoteca"){
+    alert ("contamos con los mejores profesionales para realizar la fiesta de su sueños")
+}
+else{
+    evento = prompt ("el evento elegido no es correcto. Favor elegir entre las opciones: boda, cumpleaño, empresarial, discoteca")
+}
+
+
+
+//constantes
 
 const menos50 = 150;
 const entre50y100 = 100;
 const entre100y200 = 80;
-const LaJ = 0.2 
-var costeParcial = 
+const LaJ = 0.2;
+var costeParcial = 0; 
+
+
+//calculadora
+
+let cantpers = prompt("¿para cuantas personas estima realizar el evento?" );
 
 alert("el evento sera para unas " + cantpers + " personas");
 
